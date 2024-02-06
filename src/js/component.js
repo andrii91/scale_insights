6351574a6282b91b6a220e32de9b16b04360eb6e
Andriy Lytvyn <andriy@devsalsa.com>
2024-02-06 16:07:02 +0200

    add header

--- src/js/component.js
+++ src/js/component.js
@@ -1,9 +1,36 @@
 $( document ).ready(function() {
+
+  var checkbox = document.querySelector('input[name=mode]');
+
+  checkbox.addEventListener('change', function() {
+      if(this.checked) {
+          trans()
+          document.documentElement.setAttribute('data-theme', 'dark')
+      } else {
+          trans()
+          document.documentElement.setAttribute('data-theme', 'light')
+      }
+  })
+
+  let trans = () => {
+      document.documentElement.classList.add('transition');
+      window.setTimeout(() => {
+          document.documentElement.classList.remove('transition');
+      }, 1000)
+  }
   
   $(window).scroll(function () {
     return $('.nav').toggleClass("fixed", $(window).scrollTop() > 0);
   });
 
+  $('.custom-video-play').click(function(e){
+    e.preventDefault();
+   
+    const video = document.getElementById($(this).parents('.custom-video').find('video').attr('id'));
+    video.play();
+    $(video).attr('controls', 'controls')
+    $(this).hide()
+  })
 
 }) 
 
